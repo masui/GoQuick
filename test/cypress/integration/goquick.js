efd40e1c7a7cdce58b22c08f98810c980d9c2808
Toshiyuki Masui <masui@pitecan.com>
2021-09-30 08:11:25 +0900

    登録されてないときGoogle検索するテスト

--- test/cypress/integration/goquick.js
+++ test/cypress/integration/goquick.js
@@ -2,16 +2,12 @@
 // GoQuick.orgのテスト
 //
 
-// GoQuick.org/masui でジャンプするかテストするにはどうすれば?
-
-
-const URL = 'http://GoQuick.org/' // デプロイ環境
+const GoQuick = 'http://GoQuick.org/' // デプロイ環境
 
 describe('GoQuickのテスト', () => {
-    it('GoQuickにアクセス', () => {
-	cy.visit(URL) // GoQuick.comサイトに移動
-
-	// ログイン画面になるはずなので適当に登録
+    beforeEach(() => {
+	cy.visit(GoQuick) // GoQuick.orgサイトに移動
+	// ログイン画面になるはずなので適当に登録してログイン
 	cy.get('#username')
 	    .clear()
 	    .type('masui')
@@ -19,7 +15,25 @@ describe('GoQuickのテスト', () => {
 	    .clear()
 	    .type('masui')
 	cy.get('#loginbutton').click()
+    })
+
+    it('登録してないときGoogle検索', () => {
+	const keywords = [
+	    'abcdefg',
+	    'VeryLongSearchString'
+	]
+	keywords.forEach((keyword) => {
+	    cy.request({
+		url: `http://GoQuick.org/${keyword}`,
+		followRedirect: false, // GoQuickのリダイレクトを止める
+	    }).then((res) => {
+		expect(res.status).to.eq(302) // リダイレクトのステータスコード
+		expect(res.redirectedToUrl).to.eq(`https://www.google.com/search?q=${keyword}`)
+	    })
+	})
+    })
 	
+    it('アドレスの登録とリダイレクトのテスト', () => {
 	const testEntries = [
 	    {url: 'https://scrapbox.io/masui', name: 'masui', desc: '増井のページ'},
 	    {url: 'https://example.com/', name: 'example', desc: 'Exampleページ'}
@@ -45,12 +59,13 @@ describe('GoQuickのテスト', () => {
 	    cy.get('#description')
 		.should('have.value', entry.desc)
 
+	    // https://docs.cypress.io/api/commands/request#Options
 	    cy.request({
 		url: `http://GoQuick.org/${entry.name}`,
 		followRedirect: false, // GoQuickのリダイレクトを止める
 	    }).then((res) => {
 		expect(res.status).to.eq(302) // リダイレクトのステータスコード
-		expect(res.redirectedToUrl).to.eq(entry.url) // 登録したアドレス?
+		expect(res.redirectedToUrl).to.eq(entry.url) // 登録したアドレスかチェック
 	    })
 	})
     })
