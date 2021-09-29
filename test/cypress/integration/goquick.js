682cb5447b17285369320ead3c6f7d11c0fc4cb1
Toshiyuki Masui <masui@pitecan.com>
2021-09-30 06:53:16 +0900

    リダイレクトのテストが動くようになった

--- test/cypress/integration/goquick.js
+++ test/cypress/integration/goquick.js
@@ -10,6 +10,8 @@ const URL = 'http://GoQuick.org/' // デプロイ環境
 describe('GoQuickのテスト', () => {
     it('GoQuickにアクセス', () => {
 	cy.visit(URL) // GoQuick.comサイトに移動
+
+	// ログイン画面になるはずなので適当に登録
 	cy.get('#username')
 	    .clear()
 	    .type('masui')
@@ -18,28 +20,38 @@ describe('GoQuickのテスト', () => {
 	    .type('masui')
 	cy.get('#loginbutton').click()
 	
-	cy.get('#shortname')
-	    .clear()
-	    .type('masui')
-	cy.get('#longname')
-	    .clear()
-	    .type('https://Scrapbox.io/masui')
-	cy.get('#description')
-	    .clear()
-	    .type('増井のScrapboxページ')
-	cy.get('#registerbutton').click()
+	const testEntries = [
+	    {url: 'https://scrapbox.io/masui', name: 'masui', desc: '増井のページ'},
+	    {url: 'https://example.com/', name: 'example', desc: 'Exampleページ'}
+	]
+	testEntries.forEach((entry) => {
+	    cy.get('#shortname')
+		.clear()
+		.type(entry.name)
+	    cy.get('#longname')
+		.clear()
+		.type(entry.url)
+	    cy.get('#description')
+		.clear()
+		.type(entry.desc)
+	    cy.get('#registerbutton').click()
+	
+	    // 登録できてるかチェック
+	    cy.visit(`http://GoQuick.org/${entry.name}!`)
+	    cy.get('#shortname')
+		.should('have.value', entry.name)
+	    cy.get('#longname')
+		.should('have.value', entry.url)
+	    cy.get('#description')
+		.should('have.value', entry.desc)
 
-	// 登録できてるかチェック
-	cy.visit('http://GoQuick.org/masui!')
-	cy.get('#shortname')
-	    .should('have.value', 'masui')
-	cy.get('#longname')
-	    .should('have.value', 'https://Scrapbox.io/masui')
-	cy.get('#description')
-	    .should('have.value', '増井のScrapboxページ')
+	    cy.request({
+		url: `http://GoQuick.org/${entry.name}`,
+		followRedirect: false, // GoQuickのリダイレクトを止める
+	    }).then((res) => {
+		expect(res.status).to.eq(302) // リダイレクトのステータスコード
+		expect(res.redirectedToUrl).to.eq(entry.url) // 登録したアドレス?
+	    })
+	})
     })
-
-    //it('Scrapboxにアクセス', () => {
-    // cy.visit('http://GoQuick.org/masui')
-    //})
 })
